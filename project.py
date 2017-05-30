14befe21e148e8fa5f153b3621e4058a696bccb5
Chris Graffeo <cgraffeo1@gmail.com>
2017-05-30 10:57:14 -0700

    Minor changes for heroku

--- project.py
+++ project.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, jsonify, url_for
 from flask import flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-# from database_setup import Base, State, Park, User
+from database_setup import Base, State, Park, User
 from flask import session as login_session
 import os
 import random
